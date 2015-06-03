10f22e6d0e93e354e144f9ac96d4cec7437d7384
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-06-03 21:34:56 +0200

    Adds QueueableCommand interface

---
+++ src/QueueableCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace League\Tactician\Bernard;
+
+use Bernard\Message;
+
+/**
+ * Marker interface for commands able to be sent to a queue
+ */
+interface QueueableCommand extends Message
+{
+
+}
